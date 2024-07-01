c4e77c0d440951606ac1c02eb799c51911b45a6d
Jawad Dridi <jd_dridi@hotmail.co.uk>
2024-07-01 10:14:26 +0100

    Load csv and excel files

--- retail_analysis.py
+++ retail_analysis.py
@@ -2,3 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+#Load data from csv file
+purchase_behaviour_data = pd.read_csv('QVI_purchase_behaviour.csv')
+
+#Load data from Excel file
+transaction_data = pd.read_excel('QVI_transaction_data.xlsx', sheet_name='in')
